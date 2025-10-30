e0faad6e86d94d80591da57d65764410852618d9
github94309 <70087761+github94309@users.noreply.github.com>
2025-10-30 13:44:13 +0530

    Add variables for GCP project configuration
    Define variables for GCP project configuration including project ID, region, zone, VM name, machine type, image, bucket name, disk name, and disk size.

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "damu727vm"
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+}
+
+variable "image" {
+  type    = string
+  default = "debian-cloud/debian-12"
+}
+
+variable "bucket_name" {
+  type        = string
+  default     = "gcp-storage-bucket"
+  description = "Base name for the GCS bucket"
+}
+
+variable "disk_name" {
+  type        = string
+  default     = "app-disk"
+  description = "Name of the persistent disk"
+}
+
+variable "disk_size" {
+  type        = number
+  default     = 10
+  description = "Size of the persistent disk in GB"
+}
