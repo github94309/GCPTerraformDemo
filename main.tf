5fae599e2dd8e9dddee03d7a56a530a2b655a765
github94309 <70087761+github94309@users.noreply.github.com>
2025-10-30 13:41:31 +0530

    Add Terraform configuration for GCP resources
    Adding compute engine, bucket and disk using terraform.

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# Generate a random ID for unique bucket names
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# Create a Compute Engine VM
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata = {}
+
+  tags = ["web", "dev"]
+}
+
+# Create a Google Cloud Storage bucket
+resource "google_storage_bucket" "app_bucket" {
+  name          = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
+  location      = var.region
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+}
+
+# Create a persistent disk
+resource "google_compute_disk" "app_disk" {
+  name = var.disk_name
+  type = "pd-standard"
+  zone = var.zone
+  size = var.disk_size
+}
+
+# Attach persistent disk to the VM
+resource "google_compute_attached_disk" "vm_disk_attachment" {
+  disk     = google_compute_disk.app_disk.id
+  instance = google_compute_instance.vm_instance.id
+}
